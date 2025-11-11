375168cdb880808e5bac6a046b5e65fd290880ac
saraheldahaby <sarah.eldahaby@student.giu-uni.de>
2025-11-11 09:34:22 +0200

    Implement Student class with constructors and methods
    Added a Student class with constructors and display method.

--- Student.h
+++ Student.h
@@ -6,6 +6,20 @@
 #using namespace std;
 //#write student class here
 
+class Student
+{
+    public:
+        Student();
+        virtual ~Student();
+        Student(int year,string m)::Person();
+        display()::Person();
+
+    protected:
+
+    private:
+};
+
+#endif // STUDENT_H
 
 
 
