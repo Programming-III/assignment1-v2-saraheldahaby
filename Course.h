b2923be321700ef7e6e9a3bf316cdbe9cb220d96
saraheldahaby <sarah.eldahaby@student.giu-uni.de>
2025-11-11 09:32:40 +0200

    Define Course class with constructors and methods

--- Course.h
+++ Course.h
@@ -4,7 +4,25 @@
 #include <string>
 #include "Student.h"
 using namespace std;
+
 //#write your code here
+
+class Course
+{
+    public:
+        Course();
+        virtual ~Course();
+        Course(string code,string cName,int max,Student* s,int curStudents);
+         addStudent(const Student& s);
+         displayCourseInfo();
+
+    protected:
+
+    private:
+};
+
+#endif // COURSE_H
+
 
 
 
